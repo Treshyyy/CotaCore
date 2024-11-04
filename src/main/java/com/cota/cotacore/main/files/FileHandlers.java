d873547e23a62f46e183a1cdea59373ebc9b38c1
lorinczdorakinga <lorinczdora000@gmail.com>
2024-11-04 16:58:21 +0200

    Newest release

--- src/main/java/com/cota/cotacore/main/files/FileHandlers.java
+++ src/main/java/com/cota/cotacore/main/files/FileHandlers.java
@@ -21,9 +21,9 @@ public class FileHandlers {
     public FileHandlers(String path, String fileName) {
         this.file = new File(path, fileName);
         if (!file.exists()) {
-            file.getParentFile().mkdir();
+            file.getParentFile().mkdirs();
 
-            CotaCore.INSTANCE.saveResource(fileName, false);
+            CotaCore.INSTANCE.getUserPlugin().saveResource(fileName, false);
 
         }
         this.config = YamlConfiguration.loadConfiguration(file);
